9290cef68ca5b4882387c62f6a601b27baa8233b
William Mantly <wmantly@gmail.com>
2026-08-01 01:27:07 -0400

    fix(secrets): remove hardcoded vault token for gitguardian

--- nodejs/utils/conf_manager.js
+++ nodejs/utils/conf_manager.js
@@ -1,7 +1,7 @@
 'use strict';
 const conf = require('@simpleworkjs/conf');
-const VAULT_URL = 'http://openbao:8200';
-const VAULT_TOKEN = 'root';
+const VAULT_URL = process.env.VAULT_ADDR || 'http://openbao:8200';
+const VAULT_TOKEN = process.env.VAULT_TOKEN || 'root';
 
 async function getVaultConf() {
   try {
